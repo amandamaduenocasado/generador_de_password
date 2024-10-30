63742e76b9e09e8993f761b18b0b1a7f46a967a6
amandamaduenocasado <amandacaso97@gmail.com>
2024-10-30 22:38:47 +0100

    done!

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+const array = [];
+let createPassword = '';
+
+const changePassword = document.getElementById('password');
+const generateCta = document.getElementById('generateCta');
+const lengthRange = document.getElementById('lengthRange');
+const lengthDisplay = document.getElementById('lengthDisplay'); 
+
+
+lengthRange.addEventListener('input', () => {
+    lengthDisplay.textContent = lengthRange.value; 
+});
+
+
+generateCta.addEventListener('click', () => {
+    createPassword = ''; 
+    array.length = 0; 
+
+
+    const passwordLength = lengthRange.value;
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+
+
+    for (let i = 0; i < passwordLength; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        createPassword += characters[randomIndex]; 
+}    
+    changePassword.textContent = createPassword; 
+});
