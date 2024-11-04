360a98ef4202101d15921acc609890d4ad890f64
amandamaduenocasado <amandacaso97@gmail.com>
2024-11-04 11:43:26 +0100

    done!

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,61 @@
-const array = [];
+
 let createPassword = '';
 
 const changePassword = document.getElementById('password');
-const generateCta = document.getElementById('generateCta');
+const cta = document.getElementById('cta');
 const lengthRange = document.getElementById('lengthRange');
 const lengthDisplay = document.getElementById('lengthDisplay'); 
 
+const checkboxUppercase = document.getElementById('checkbox-1');
+const checkboxLowercase = document.getElementById('checkbox-2');
+const checkboxNumber = document.getElementById('checkbox-3');
+const checkboxSymbols = document.getElementById('checkbox-4');
 
 lengthRange.addEventListener('input', () => {
     lengthDisplay.textContent = lengthRange.value; 
 });
 
-
-generateCta.addEventListener('click', () => {
+cta.addEventListener('click', () => {
     createPassword = ''; 
-
     const passwordLength = lengthRange.value;
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+    let characters = '';
 
+    if (checkboxUppercase.checked) {
+        characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+
+    if (checkboxLowercase.checked) {
+        characters += 'abcdefghijklmnopqrstuvwxyz'
+    }
+
+    if (checkboxNumber.checked) {
+        characters += '1234567890'
+    }
+
+    if (checkboxSymbols.checked) {
+        characters += '!@#$%^&*()'
+    }
 
     for (let i = 0; i < passwordLength; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
         createPassword += characters[randomIndex]; 
-}    
+    }    
     changePassword.textContent = createPassword; 
 });
+
+
+const checkboxActive = () => {
+    if (checkboxUppercase.checked || checkboxLowercase.checked || checkboxNumber.checked || checkboxSymbols.checked) {
+        cta.disabled = false; 
+    } else {
+        cta.disabled = true; 
+    }
+}
+
+checkboxUppercase.addEventListener('change', checkboxActive);
+checkboxLowercase.addEventListener('change', checkboxActive);
+checkboxNumber.addEventListener('change', checkboxActive);
+checkboxSymbols.addEventListener('change', checkboxActive);
+
+checkboxActive();
+
